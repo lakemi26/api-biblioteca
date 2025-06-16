5b870924f602979e8bd8b3a47f8159f15017c290
lakemi26 <lakemi@outlook.com.br>
2025-06-16 18:04:35 -0300

    Feat: firebase

--- src/database/firebase.ts
+++ src/database/firebase.ts
@@ -1,8 +1,11 @@
 import admin from "firebase-admin";
 import serviceAccount  from "../../firebase-service-account.json";
 
-admin.initializeApp({
+if(!admin.apps.length){
+    admin.initializeApp({
     credential:admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
+}
+
 
 export const db = admin.firestore;
